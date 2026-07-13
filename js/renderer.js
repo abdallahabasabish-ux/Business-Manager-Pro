99c04f3c60625cec28f758d6a289c41624c87060
Abdallah Abas <abdallahabasabish@gmail.com>
2026-07-13 09:47:37 +0300

    Add password toggle functionality in renderer.js

---
+++ js/renderer.js
@@ -0,0 +1,14 @@
+// داخل renderer.js - عند تحميل الصفحة
+if (togglePassword) {
+    togglePassword.addEventListener('click', () => {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+        // تغيير أيقونة العين
+        const icon = togglePassword.querySelector('i');
+        if (type === 'password') {
+            icon.className = 'fas fa-eye';
+        } else {
+            icon.className = 'fas fa-eye-slash';
+        }
+    });
+}
